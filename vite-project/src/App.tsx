c193787808146d5d2248fee675704e8ff607ef6a
Luisde Cespedes <lewisofbristol@gmail.com>
2024-09-11 15:19:09 +0200

    SearchBar done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,16 +5,46 @@ import "./App.css";
 import myImage from "../public/lens.png";
 
 function App() {
+
+  const [city, setCity] = useState("");
+
+  function handleTyping (event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit (event) {
+    event.preventDefault();
+    console.log(city)
+  }
+
+
   return (
-    <>
+    <div className="MacroContainer">
+
+      
       <nav>
         <form>
-          <img src={myImage}></img>
-          <input type="text" placeholder="Enter your City"></input>
-          <button>Search</button>
+
+        <img src={myImage}></img>
+
+          <input value={city} onChange={handleTyping}  type="text" placeholder="Enter your City">
+          </input>
+
+          <button onClick={handleSubmit}>Search</button>
+
         </form>
       </nav>
-    </>
+
+      <div className="WeatherImage">
+      </div>
+
+      <div className="WeatherInfo">
+      </div>
+
+
+
+
+    </div>
   );
 }
 
